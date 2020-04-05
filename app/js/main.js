6f67ac0f52a9dd473ba9e3fcba62e08f1972c653
Irina <strelaweb@gmail.com>
2020-04-05 17:11:05 +1000

    added slider and stocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
 $(function(){
-
+	$('.slider__items').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  
+	  asNavFor: '.slider__nav'
+	});
+	$('.slider__nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider__items',
+	  vertical: true,
+	  infinite: true,
+	  verticalSwiping: true,
+	  dots: false,
+	  arrows:true,
+	  /*responsive: [
+	    {
+	      breakpoint: 1400,
+	      settings: {
+	          slidesToShow: 3,
+	          slidesToScroll: 1,
+          
+	      }
+	    }
+	    ]*/
+	  
+	});
 
 });
