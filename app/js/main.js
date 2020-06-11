428b48e5e6358ca5a05c8eb1c27f5b201430ed3c
Irina <strelaweb@gmail.com>
2020-06-11 18:26:28 +1000

    added changes

--- app/js/main.js
+++ app/js/main.js
@@ -27,4 +27,18 @@ $(function(){
     $('.menu-burger').on('click',function(){
     	$('.menu-burger').toggleClass('active');
     });
+
+    $('.nav__menu-link').on('click', function() {
+
+	    let href = $(this).attr('href');
+
+	    $('html, body').animate({
+	        scrollTop: $(href).offset().top
+	    }, {
+	        duration: 470,   // по умолчанию «400»
+	        easing: "linear" // по умолчанию «swing»
+	    });
+	     
+	    return false;
+	});
 });
